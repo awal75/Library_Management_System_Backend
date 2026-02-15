4e8356638b0e3b9fc783e47153a2aa177e900e35
Awal Islam <awal.aaraf92@gmail.com>
2026-02-16 01:25:51 +0600

    add Book,Author,Member & BorrowRecord serializer

---
+++ library/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from library import models
+
+
+class BookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model=models.Book
+        fields=['id','title','author','ISBN','category','availability_status']
+
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Author
+        fields=['id','name','biography']
+
+
+class MemberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.Member
+        fields=['id','name','email','membership_date']
+
+
+class BorrowRecordSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=models.BorrowRecord
+        fields=['id','book','member','borrow_date','return_date']
+        
--- library/views.py
+++ library/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from rest_framework.viewsets import ModelViewSet
+from library import serializers
+from library import models
 # Create your views here.
+
+
+class BookModelViewSet(ModelViewSet):
+    serializer_class=serializers.BookSerializer
+    queryset=models.Book.objects.all()
+
+
+
+class MemberModelViewSet(ModelViewSet):
+    serializer_class=serializers.MemberSerializer
+    queryset=models.Member.objects.all()
+
+
+class AuthorModelViewSet(ModelViewSet):
+    serializer_class=serializers.AuthorSerializer
+    queryset=models.Author.objects.all()
+
+
+class BorrowRecordModelViewSet(ModelViewSet):
+    serializer_class=serializers.BorrowRecordSerializer
+    queryset=models.BorrowRecord.objects.all()
